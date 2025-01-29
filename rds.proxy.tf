17a0022c8122662d7ffb12c941885f8a47a93253
Rogerio Calixto.futura@gmail.com <rogerio.calixto@futurasistemas.com.br>
2025-01-29 14:17:19 -0300

    comendados recursos extras do RDS

--- rds.proxy.tf
+++ rds.proxy.tf
@@ -1,58 +1,58 @@
-resource "aws_iam_role" "rds-proxy-role" {
-  name = "rds_proxy_role"
+# resource "aws_iam_role" "rds-proxy-role" {
+#   name = "rds_proxy_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "rds.amazonaws.com"
-        }
-      },
-    ],
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "rds.amazonaws.com"
+#         }
+#       },
+#     ],
+#   })
+# }
 
-resource "aws_iam_policy" "rds-proxy-policy" {
-  name        = "rds_proxy_policy"
-  description = "Permissao de acesso ao Secrets de RDS"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "secretsmanager:GetSecretValue"
-        ],
-        Resource = "${aws_secretsmanager_secret.secret-rds.arn}"
-      },
-    ],
-  })
-  depends_on = [aws_secretsmanager_secret.secret-rds]
-}
+# resource "aws_iam_policy" "rds-proxy-policy" {
+#   name        = "rds_proxy_policy"
+#   description = "Permissao de acesso ao Secrets de RDS"
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "secretsmanager:GetSecretValue"
+#         ],
+#         Resource = "${aws_secretsmanager_secret.secret-rds.arn}"
+#       },
+#     ],
+#   })
+#   depends_on = [aws_secretsmanager_secret.secret-rds]
+# }
 
-resource "aws_iam_role_policy_attachment" "rds-proxy-role-policy-attachment" {
-  policy_arn = aws_iam_policy.rds-proxy-policy.arn
-  role       = aws_iam_role.rds-proxy-role.name
-}
+# resource "aws_iam_role_policy_attachment" "rds-proxy-role-policy-attachment" {
+#   policy_arn = aws_iam_policy.rds-proxy-policy.arn
+#   role       = aws_iam_role.rds-proxy-role.name
+# }
 
-resource "aws_db_proxy" "rds-proxy" {
-  name                   = "rds-proxy"
-  debug_logging          = false
-  engine_family          = "POSTGRESQL"
-  idle_client_timeout    = 300
-  require_tls            = true
-  role_arn               = aws_iam_role.rds-proxy-role.arn
-  vpc_security_group_ids = [aws_security_group.sg-rds.id]
-  vpc_subnet_ids         = module.vpc.public-subnet_ids
+# resource "aws_db_proxy" "rds-proxy" {
+#   name                   = "rds-proxy"
+#   debug_logging          = false
+#   engine_family          = "POSTGRESQL"
+#   idle_client_timeout    = 300
+#   require_tls            = true
+#   role_arn               = aws_iam_role.rds-proxy-role.arn
+#   vpc_security_group_ids = [aws_security_group.sg-rds.id]
+#   vpc_subnet_ids         = module.vpc.public-subnet_ids
 
-  auth {
-    auth_scheme = "SECRETS"
-    iam_auth    = "DISABLED"
-    secret_arn  = aws_secretsmanager_secret.secret-rds.arn
-  }
+#   auth {
+#     auth_scheme = "SECRETS"
+#     iam_auth    = "DISABLED"
+#     secret_arn  = aws_secretsmanager_secret.secret-rds.arn
+#   }
 
-  depends_on = [aws_secretsmanager_secret.secret-rds]
-}
+#   depends_on = [aws_secretsmanager_secret.secret-rds]
+# }
--- rds.replica.tf
+++ rds.replica.tf
@@ -1,23 +1,23 @@
-#Criando RDS Replica
+# #Criando RDS Replica
 
-resource "aws_db_instance" "rds_db_replica" {
-  skip_final_snapshot     = true
-  replicate_source_db     = aws_db_instance.rds_db.identifier
-  parameter_group_name    = aws_db_parameter_group.rds_db_parameter_group.name
-  instance_class          = "db.t3.small"
-  identifier              = "${var.identifier}-replica"
-  backup_retention_period = 7
-  apply_immediately       = true
+# resource "aws_db_instance" "rds_db_replica" {
+#   skip_final_snapshot     = true
+#   replicate_source_db     = aws_db_instance.rds_db.identifier
+#   parameter_group_name    = aws_db_parameter_group.rds_db_parameter_group.name
+#   instance_class          = "db.t3.small"
+#   identifier              = "${var.identifier}-replica"
+#   backup_retention_period = 7
+#   apply_immediately       = true
 
-  vpc_security_group_ids = [aws_security_group.sg-rds.id]
+#   vpc_security_group_ids = [aws_security_group.sg-rds.id]
 
-  tags = {
-    Project     = local.projeto
-    Environment = var.ambiente
-    Replica     = "Yes"
-  }
+#   tags = {
+#     Project     = local.projeto
+#     Environment = var.ambiente
+#     Replica     = "Yes"
+#   }
 
-  lifecycle {
-    ignore_changes = [tags, deletion_protection]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [tags, deletion_protection]
+#   }
+# }
